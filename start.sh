265df3d5eee5fcb7d383e29c342f08c0afc345c9
fercho <fpaniagua2023@incos-santacruz.edu.bo>
2026-07-23 00:35:47 -0400

    Configurar start.sh y .env para producción

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Generar documentación de Swagger al inicio (cuando la BD está disponible)
-php artisan l5-swagger:generate
-
-# Optimizar Laravel
+# Optimizar Laravel (esto debe ir ANTES de generar Swagger)
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
+
+# Generar documentación de Swagger (después de las optimizaciones)
+php artisan l5-swagger:generate
 
 # Iniciar el servidor
 php artisan serve --host=0.0.0.0 --port=10000
